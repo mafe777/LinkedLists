e96c6a8eaa233fd5c42794925bca19df810903bc
mafe <Usuario@DESKTOP-PDI6DA1>
2023-04-19 21:22:31 -0500

    menu

--- LinkedLists.UI/Program.cs
+++ LinkedLists.UI/Program.cs
@@ -1,11 +1,49 @@
 ﻿
 using LinkedLists.Logic;
+using System.ComponentModel.Design;
 
 Console.WriteLine("*-----Linked Lists-----*");
 var singlelist = new SingleList<string>();
-singlelist.Add("mafe");    
-singlelist.Add("maria");    
-singlelist.Add("jesus");    
-singlelist.Add("ana");    
-singlelist.Add("fernando");
-Console.WriteLine(singlelist);
+
+
+int option; 
+do
+{
+    option = Menu();
+    switch (option)
+    {
+        case 1: AddItem(); break;
+        case 2: ShowList(); break;
+        default:break;
+    }
+} while (option !=0);
+
+void ShowList()
+{
+    if (singlelist.IsEmpty) 
+    {
+        Console.WriteLine("The List Is Empty");
+        return;
+    }
+    Console.WriteLine("The List Elements Are: ");
+    Console.WriteLine(singlelist);
+
+}
+
+void AddItem()
+{
+    Console.WriteLine("Enter The Item");
+    var item = Console.ReadLine();
+    singlelist.Add(item!);
+}
+
+
+int Menu()
+{
+    Console.WriteLine("1. Add Item");
+    Console.WriteLine("2. Show List");
+    Console.WriteLine("0. Exit");
+    Console.Write("choose the option...");
+    var option = Console.ReadLine();
+    return Convert.ToInt32(option);
+}
